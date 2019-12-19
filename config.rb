273dbb5dfb9075cc30b1d01c7f86f32de49c8f56
Matt Moretti <werebus@gmail.com>
2019-12-19 17:23:22 -0500

    Use webpacker for SASS too.

--- config.rb
+++ config.rb
@@ -1,14 +1,5 @@
 require 'pathname'
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
 webpack_build = './node_modules/webpack/bin/webpack.js --bail'
 webpack_watch = './node_modules/webpack/bin/webpack.js --watch -d'
 activate :external_pipeline,
@@ -16,5 +7,3 @@ activate :external_pipeline,
   command: build? ? webpack_build : webpack_watch,
   source: '.tmp/dist',
   latency: 1
-
-::SassC.load_paths.concat [Pathname(__dir__).join('node_modules')]
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    site: './source/javascripts/site.js'
+    js: './javascripts/site.js',
+    css: './stylesheets/site.css.scss'
   },
 
   resolve: {
@@ -14,4 +15,25 @@ module.exports = {
     path: path.join(__dirname, '.tmp', 'dist'),
     filename: 'javascripts/[name].js',
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [require('autoprefixer')];
+              }
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ],
+  }
 };
