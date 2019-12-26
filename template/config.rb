30b54edbe68795114c24241c790bd06a9fd9451c
Matt Moretti <werebus@gmail.com>
2019-12-26 16:49:13 -0500

    Markdown rendering

--- template/config.rb
+++ template/config.rb
@@ -4,6 +4,16 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 page '/*.xml', layout: false
 
+set :markdown,
+  autolink: true,
+  fenced_code_blocks: true,
+  highlight: true,
+  smartypants: true,
+  strikethrough: true,
+  tables: true,
+  with_toc_data: true
+set :markdown_engine, :redcarpet
+
 webpack_build = './node_modules/webpack/bin/webpack.js --bail -p'
 webpack_watch = './node_modules/webpack/bin/webpack.js --watch -d'
 activate :external_pipeline,
